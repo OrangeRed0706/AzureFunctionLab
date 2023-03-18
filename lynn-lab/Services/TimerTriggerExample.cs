b37f3d188c3b3b588d227dd9570b05933ed203eb
Lynn <59608151+OrangeRed0706@users.noreply.github.com>
2023-03-18 20:45:55 +0800

    fix

--- lynn-lab/Services/TimerTriggerExample.cs
+++ lynn-lab/Services/TimerTriggerExample.cs
@@ -7,21 +7,21 @@ namespace lynn_lab.Services;
 
 public sealed class TimerTriggerExample
 {
-    private readonly ILogger _logger;
     private readonly LineHelper _lineHelper;
 
-    public TimerTriggerExample(ILogger logger, LineHelper lineHelper)
+    public TimerTriggerExample(LineHelper lineHelper)
     {
-        _logger = logger;
         _lineHelper = lineHelper;
     }
 
     [Function("TimerTriggerExample")]
     public async Task Run([TimerTrigger("0 */5 * * * *")] MyInfo myTimer, FunctionContext context)
     {
+        var logger = context.GetLogger("HttpTriggerExample");
+
         await _lineHelper.SendLineNotify(
             $"C# Timer trigger function executed at: {DateTime.Now},Next timer schedule at: {myTimer.ScheduleStatus.Next}");
-        _logger.LogInformation("C# Timer trigger function executed at: {Now}", DateTime.Now);
-        _logger.LogInformation("Next timer schedule at: {ScheduleStatusNext}", myTimer.ScheduleStatus.Next);
+        logger.LogInformation("C# Timer trigger function executed at: {Now}", DateTime.Now);
+        logger.LogInformation("Next timer schedule at: {ScheduleStatusNext}", myTimer.ScheduleStatus.Next);
     }
 }
