86a5041214df05161d162426b203a2c45b525217
Lynn <59608151+OrangeRed0706@users.noreply.github.com>
2023-03-18 17:19:37 +0800

    init

---
+++ FunctionLab/Run.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace Company.FunctionApp1;
+
+public static class Run
+{
+    [Function("Run")]
+    public static void Run([TimerTrigger("0 */5 * * * *")] MyInfo myTimer, FunctionContext context)
+    {
+        var logger = context.GetLogger("Run");
+        logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+        logger.LogInformation($"Next timer schedule at: {myTimer.ScheduleStatus.Next}");
+        
+    }
+}
+
+public class MyInfo
+{
+    public MyScheduleStatus ScheduleStatus { get; set; }
+
+    public bool IsPastDue { get; set; }
+}
+
+public class MyScheduleStatus
+{
+    public DateTime Last { get; set; }
+
+    public DateTime Next { get; set; }
+
+    public DateTime LastUpdated { get; set; }
+}
