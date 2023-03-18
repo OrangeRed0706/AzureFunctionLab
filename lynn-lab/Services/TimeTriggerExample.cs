4251986ce3fec5313c57b057d84892530049d481
Lynn <59608151+OrangeRed0706@users.noreply.github.com>
2023-03-19 01:56:11 +0800

    test

--- lynn-lab/Services/TimeTriggerExample.cs
+++ lynn-lab/Services/TimeTriggerExample.cs
@@ -16,28 +16,21 @@ public class TimeTriggerExample
     }
 
     [Function("TimeTriggerExample")]
-    public async Task Run([TimerTrigger("0 */1 * * * *")] MyInfo myTimer, FunctionContext context)
+    public async Task Run([TimerTrigger("0 */10 * * * *")] MyInfo myTimer, FunctionContext context)
     {
-        var utcTime = DateTime.UtcNow;
-        var localTime = ToTaipeiTime(utcTime);
+        var utcTime = DateTimeOffset.UtcNow.AddHours(8);
         var logger = context.GetLogger("TimeTriggerExample1");
 
         await _lineHelper.SendLineNotify(
-            $"\n C# Timer trigger function executed at: {localTime}");
+            $"\n C# Timer trigger function executed at: {utcTime},\n Next timer schedule at: {myTimer.ScheduleStatus.Next}");
         var option = new JsonSerializerOptions()
         {
             WriteIndented = true
         };
-        // logger.LogInformation($"MyInfo: {JsonSerializer.Serialize(myTimer, option)}");
-        // logger.LogInformation($"Function Context: {JsonSerializer.Serialize(context)}");
-        logger.LogInformation("C# Timer trigger function executed at: {LocalTime})", localTime);
-        // logger.LogInformation("Next timer schedule at: {LocalTime}", ToTaipeiTime(myTimer.ScheduleStatus.Next));
-    }
-
-    private DateTime ToTaipeiTime(DateTime time)
-    {
-        var timeZoneInfo = TimeZoneInfo.FindSystemTimeZoneById("Taipei Standard Time"); // 指定時區
-        return TimeZoneInfo.ConvertTimeFromUtc(time, timeZoneInfo);
+        logger.LogInformation("MyInfo: {Serialize}", JsonSerializer.Serialize(myTimer, option));
+        logger.LogInformation("Function Context: {Serialize}", JsonSerializer.Serialize(context));
+        logger.LogInformation("C# Timer trigger function executed at: {LocalTime})", utcTime);
+        logger.LogInformation("Next timer schedule at: {LocalTime}", myTimer.ScheduleStatus.Next);
     }
 }
 
